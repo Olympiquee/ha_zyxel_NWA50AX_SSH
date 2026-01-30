56e72d7fca229393d821dee426670415e2de0638
Olympiquee <72965419+Olympiquee@users.noreply.github.com>
2026-01-30 16:25:16 +0100

    Delete custom_components/ha_zyxel directory

--- custom_components/ha_zyxel/const.py
+++
@@ -1,9 +0,0 @@
-DOMAIN = "ha_zyxel"
-DEFAULT_NAME = "Zyxel Device"
-DEFAULT_HOST = "https://192.168.1.1"
-DEFAULT_USERNAME = "admin"
-DEFAULT_SCAN_INTERVAL = 30
-
-CONF_HOST = "host"
-CONF_USERNAME = "username"
-CONF_PASSWORD = "password"
